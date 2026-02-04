755a7c5b74ad671fd44b8342c261ff39fc874caa
RomuloSHY <stin.huaman.yalo@gmail.com>
2026-02-04 18:15:17 -0500

    Agrega proyecto springboot + maven

---
+++ backend-springboot/sistema-futbol-admin/src/main/java/com/admin/futbol/sistemafutboladmin/SistemaFutbolAdminApplication.java
@@ -0,0 +1,13 @@
+package com.admin.futbol.sistemafutboladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFutbolAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaFutbolAdminApplication.class, args);
+    }
+
+}
---
+++ backend-springboot/sistema-futbol-admin/src/test/java/com/admin/futbol/sistemafutboladmin/SistemaFutbolAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.futbol.sistemafutboladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFutbolAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
